35ef9d9a83ce5a8218e7e09108312c33b28b85a9
Ben Davis <bdavis493@byui.edu>
2023-02-23 12:24:19 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,43 @@
-import logo from './logo.svg';
 import './App.css';
+import Goals  from "./Goals"
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Outlet,
+  Link
+} from "react-router-dom";
 
 function App() {
+
+  const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <div id="detail">
+        <h1>Hello World</h1>
+        {/* <Outlet /> */}
+
+    </div>
+    ),
+    errorElement: <h1>That was a bad idea</h1>
+    // children: [
+    //   {
+    //     path:"goals",
+    //     element: <Goals />
+    //   }
+    // ]
+  },
+  {
+    path: "goals",
+    element: <Goals />
+  }
+]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+         <RouterProvider router={router} />
+         {/* <Link to={'goals'}>Goals</Link> */}
       </header>
     </div>
   );
---
+++ src/Goals.js
@@ -0,0 +1,9 @@
+
+function Goals() {
+    return (
+        <h1>
+            Goals
+        </h1>
+    )
+}
+export default Goals;
